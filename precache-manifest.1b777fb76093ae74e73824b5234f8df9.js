e2c75aa5296a4de58bc0c94c17fb3019d35089eb
Victor Stanchevici <vstanchevici@yahoo.com>
2022-07-08 11:27:02 +0300

    update 2d v1

---
+++ precache-manifest.1b777fb76093ae74e73824b5234f8df9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbdeec6eaa59413421ec4ff1fa9e9478",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73f548dd8a791ba22366",
+    "url": "/static/css/2.77f6eca7.chunk.css"
+  },
+  {
+    "revision": "8df27cf10fbff82d83f9",
+    "url": "/static/css/main.43c71864.chunk.css"
+  },
+  {
+    "revision": "73f548dd8a791ba22366",
+    "url": "/static/js/2.1a74dcb1.chunk.js"
+  },
+  {
+    "revision": "d3e49d62c5e83bd760c7639e00b15fb0",
+    "url": "/static/js/2.1a74dcb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faa0650cd795eae4f8f5",
+    "url": "/static/js/3.0ef43b14.chunk.js"
+  },
+  {
+    "revision": "8df27cf10fbff82d83f9",
+    "url": "/static/js/main.bdfaef1c.chunk.js"
+  },
+  {
+    "revision": "07363369146e588f6eae",
+    "url": "/static/js/runtime~main.20e9a8d0.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
