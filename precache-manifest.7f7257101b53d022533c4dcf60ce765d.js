3dff56751e85c2963616f912dec5ed529d86bc4f
Rusnac-Ivan <75523390+Rusnac-Ivan@users.noreply.github.com>
2022-07-11 14:36:38 +0300

    update wasm and resources

---
+++ precache-manifest.7f7257101b53d022533c4dcf60ce765d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd27b08207d2bcb33a8ed2813cd19443",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2cc92c5865070074ce4f",
+    "url": "./static/css/2.7471d5ba.chunk.css"
+  },
+  {
+    "revision": "60ef746d3d507e6e0c9c",
+    "url": "./static/css/main.80089a72.chunk.css"
+  },
+  {
+    "revision": "2cc92c5865070074ce4f",
+    "url": "./static/js/2.96758eca.chunk.js"
+  },
+  {
+    "revision": "d3e49d62c5e83bd760c7639e00b15fb0",
+    "url": "./static/js/2.96758eca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4e1bb90ed99a1f45a33",
+    "url": "./static/js/3.b83ec77d.chunk.js"
+  },
+  {
+    "revision": "60ef746d3d507e6e0c9c",
+    "url": "./static/js/main.70f0d989.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/main.70f0d989.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96b040e0961d935dc815",
+    "url": "./static/js/runtime~main.0b045f08.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "./static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "./static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "./static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "./static/media/slick.f97e3bbf.svg"
+  }
+]);
