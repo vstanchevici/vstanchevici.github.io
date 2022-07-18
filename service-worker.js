27a94325efe5562d261cdf7cb2cdda29bfa9356f
Victor Stanchevici <vstanchevici@yahoo.com>
2022-07-18 17:02:51 +0300

    adding new version with scrooling

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a0697a4f026d6b6c0b7121bc94d7723a.js"
+  "/precache-manifest.82707bde3cdacc4a058bfc03af64ec26.js"
 );
 
 self.addEventListener('message', (event) => {
